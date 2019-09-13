aa0e5504217b2c7b2fd85753a705e8bf38b3fd0c
Stephen Jeong <stephen.h.jeong@gmail.com>
2019-09-12 20:06:02 -0700

    Add code in server to handle bundles from different services

--- server/index.js
+++ server/index.js
@@ -1,13 +1,70 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const compression = require('compression');
+const axios = require('axios');
 const port = 3006;
 
 const app = express();
 
 app.use('/', express.static('public'));
 app.use('/:gameId', express.static('public'));
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// write code to handle getting data
+let overview = 'http://localhost:3000';
+let reviews = 'http://localhost:3001';
+let image = 'http://localhost:3002';
+let aboutGame = 'http://localhost:3003';
+let topReviews = 'http:localhost:3004';
+
+app.use('/api/:service/bundle.js', (req, res) => {
+  let service = req.params.service;
+  console.log(`request for ${service}`);
+  if (service === 'overview') {
+    axios.get(`${overview}/1/dist/bundle.js`)
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((err) => {
+        console.log(`error in request for ${service}: ${err}`);
+      })
+  } else if (service === 'reviews') {
+    axios.get(`${overview}/Stardew%20Valley/dist/bundle.js`)
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((err) => {
+        console.log(`error in request for ${service}: ${err}`);
+      })
+  } else if (service === 'image') {
+    axios.get(`${overview}/1/stardew_valley/dist/bundle.js`)
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((err) => {
+        console.log(`error in request for ${service}: ${err}`);
+      })
+  } else if (service === 'aboutGame') {
+    axios.get(`${overview}/1/bundle.js`)
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((err) => {
+        console.log(`error in request for ${service}: ${err}`);
+      })
+  } else if (service === 'topReviews') {
+    axios.get(`${overview}/1/bundle.js`)
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((err) => {
+        console.log(`error in request for ${service}: ${err}`);
+      })
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`App is listening on ${port}`);
