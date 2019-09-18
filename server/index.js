e32c8bcc47aaedf29f0d198628dc56eb392676b0
Stephen Jeong <stephen.h.jeong@gmail.com>
2019-09-17 19:17:58 -0700

    Remove topreview component

--- server/index.js
+++ server/index.js
@@ -14,10 +14,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // write code to handle getting data
 let overview = 'http://ec2-18-144-35-234.us-west-1.compute.amazonaws.com';
-let reviews = 'http://ec2-54-67-60-167.us-west-1.compute.amazonaws.com';
+let reviews = 'http://ec2-54-183-55-106.us-west-1.compute.amazonaws.com';
 let image = 'http://ec2-13-57-33-155.us-west-1.compute.amazonaws.com';
 let aboutGame = 'http://my-env.wf8cymc59m.us-west-1.elasticbeanstalk.com';
-let topReviews = 'http:localhost:3004';
 
 app.use('/api/:service', (req, res) => {
   let service = req.params.service;
@@ -55,17 +54,8 @@ app.use('/api/:service', (req, res) => {
       .catch((err) => {
         console.log(`error in request for ${service}: ${err}`);
       })
-  } else if (service === 'topReviews') {
-    axios.get(`${topReviews}/1/bundle.js`)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(`error in request for ${service}: ${err}`);
-      })
   }
 })
-
 
 app.listen(port, () => {
   console.log(`App is listening on ${port}`);
